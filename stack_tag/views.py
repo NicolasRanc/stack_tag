61eb1d7f87300cf17b899cf91960e0b90b244ec5
NicolasRanc <ranc.nicolas@gmail.com>
2018-08-29 18:09:20 +0200

    Bug solving2

--- stack_tag/views.py
+++ stack_tag/views.py
@@ -22,3 +22,5 @@ def get_quest(quest):
 
 #curl -i http://frssldev01:5000/question/This%20is%20a%20test%20for%20Python%20&lt;strong&gt;Flask%20API&lt;/strong&gt;%20development%20where%20&lt;code&gt;%20toto%20&lt;/code&gt;.
 #curl -i http://frssldev01:5000/question/This%20is%20a%20test%20for%20Python
+
+#curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
